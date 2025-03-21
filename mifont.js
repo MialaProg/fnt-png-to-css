246baa3a521109e1c89426283f5ac165d8ee1a60
Miala <miala+gitBdg@ik.me>
2025-03-21 21:43:51 +0100

    wi

---
+++ mifont.js
@@ -0,0 +1,31 @@
+// Set properties of the MiFont as size, right, adaptR and bottom
+function setMiFontProp(prop, value) {
+    document.documentElement.style.setProperty('--mifont-' + prop, value);
+}
+
+function miFontToConvert(obj){
+    preview.classList.add('mifont');
+    preview.classList.remove('mifont');
+}
+
+
+function convertMiFonts() {
+    const miFontObjs = document.querySelectorAll('.mifont');
+    miFontObjs.forEach(miFontObj => {
+        if (!miFontObj.classList.contains('convertedIntoMialaFont')) {
+            const text = miFontObj.innerHTML;
+            miFontObj.setAttribute('initHTML', text);
+            const output = text.split('').map(c => {
+                if (c == "\n") {
+                    return "<br>";
+                }
+                const code = c.charCodeAt(0);
+                const char = currentChars.find(ch => ch.id === code);
+                return char ? `<span class="mibmp-font char-${code}"></span>` : `<span style="margin-right: calc(1em*var(--mifont-right));"></span>`;
+            }).join('');
+
+            miFontObj.innerHTML = output;
+            miFontObj.classList.add('convertedIntoMialaFont');
+        }
+    });
+}
